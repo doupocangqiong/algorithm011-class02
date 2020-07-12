243740ecc65c8c2af54e38fdb18258317202d3a1
kubernete <34005352+kubernete@users.noreply.github.com>
2020-07-12 18:54:59 +0800

    二叉树的最近公共祖先

---
+++ Week_03/LowestCommonAncestor.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param TreeNode $root
+     * @param TreeNode $p
+     * @param TreeNode $q
+     * @return TreeNode
+     */
+    function lowestCommonAncestor($root, $p, $q) {
+        if($root == null) return null;
+        if($root == $p || $root == $q) return $root;
+        $left = $this->lowestCommonAncestor($root->left, $p, $q);
+        $right = $this->lowestCommonAncestor($root->right, $p, $q);
+        if(!$left) return $right;
+        if(!$right) return $left;
+        if($left && $right) return $root;
+        return null;
+    }
+}
